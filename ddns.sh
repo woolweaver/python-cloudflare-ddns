fb89e893f06af1aa4298aaf3c705e7c90f6091ea
Michael Woolweaver <m.woolweaver@icloud.com>
2020-04-29 02:06:11 -0500

    Logic seems broken but the created link works
    will fix logic when I wake up

---
+++ ddns.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# change these 
+zoneID=<Zone_ID>
+dnsID=<DNS_ID>
+email="<email_used_for_login>"
+key=<Global_API_Key>
+
+
+# fetch IP address
+getIP=$(wget https://myip.dnsomatic.com/ -q -O -)
+ip=`cat ip.txt`
+
+if [ "$getIP" != "$ip" ]
+  echo $getIP > ip.txt
+  curl -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneID/dns_records/$dnsID" -H "X-Auth-Email: $email" -H "X-Auth-Key: $key" -H "Content-Type: application/json" --data '{"type":"A","name":"pi-hole.woolweaver.bid","content":"'$getIP'","ttl":120,"proxied":true}'
+elif [ "$getIP = "$ip" ]
+  echo "IP address hasn't changed since we ran last."
+else
+  echo "something is broken"
