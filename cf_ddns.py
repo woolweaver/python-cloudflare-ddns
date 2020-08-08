5417f58d0ed0201b21e9a5d0fa274810b25a9b43
mwoolweaver <michael@woolweaver.bid>
2020-08-08 14:31:52 -0500

    split out config data

--- cf_ddns.py
+++ cf_ddns.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 from requests import get, put
-# change these as you need please see included links for more details
-zoneID = 'Zone-ID-For-Domain' # Getting Zone ID's https://api.cloudflare.com/#zone-list-zones
-dnsID = 'DNS-ID-For-SubDomain' # Getting DNS Record ID's https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
-# Put together our Header
-headers = {
-    'X-Auth-Email': 'Your-Email-Address', # The email you use to login into Cloudflare
-    'X-Auth-Key': 'Global-API-Key', # The Global API Key found here --> https://dash.cloudflare.com/profile/api-tokens
-    'Content-Type': 'application/json' # Type of data to be passed must be application/json
-}
+from config.config import headers, zoneID, dnsID
 
 # We need to find our IP
 ip = get('https://diagnostic.opendns.com/myip') # fetch our current IP address
 current_ip = ip.text # we only need the text on the page
 
-# Put together our Cloudflare URL with our zoneID and our dnsID
+# Put together our Cloudflare URL with our zoneID and our dnsID --> https://api.cloudflare.com/#dns-records-for-a-zone-dns-record-details
 url = "https://api.cloudflare.com/client/v4/zones/" + zoneID + "/dns_records/" + dnsID
 
-# Find Current Cloudflare IP address
-check_cf_IP = get(url, headers=headers) # GET Info for the exact domain we want to update
-check = check_cf_IP.json() # convert to json
+# Find Current Cloudflare domain settins
+check_cf = get(url, headers=headers) # GET Info for the exact domain we want to update
+check = check_cf.json() # convert to json
 
 if check["success"] == True: # check for success
     current_cf_ip = check["result"]["content"] # Find the IP that Cloudflare has right now
@@ -27,6 +19,8 @@ if check["success"] == True: # check for success
     ttl = check["result"]["ttl"] # Get TTL
     record_type = check["result"]["type"] # get record type
     proxy = check["result"]["proxied"] # get proxy status
+
+    # Create the data we want to pass to Cloudflare API
     data = {
         # https://api.cloudflare.com/#dns-records-for-a-zone-update-dns-record
         "type":record_type, # record type A or AAA. We just pass what is alreay set.
@@ -36,17 +30,17 @@ if check["success"] == True: # check for success
         "proxied":proxy # whether or not to proxy site thru cloudflare. Depends on your needs. True or False. We just pass what is already set.
         # See here --> https://support.cloudflare.com/hc/en-us/articles/200169156-Identifying-network-ports-compatible-with-Cloudflare-s-proxy
     }
-    # Show Our Current IP Addresses
-    print ("Current Cloudflare IP: " + current_cf_ip + "\n")
-    print ("Your Current IP  : " + current_ip + "\n")
+
     # Logic to check our IP Addresses
     if current_cf_ip == current_ip: # Check if our current IP matches what Cloudflare has
         print ("IP address hasn't changed") # If it matches we don't need to change it
+        print ("Date last modified: " + check["result"]["modified_on"] + "\n")
     else: # If it has changed update it
         response = put(url, headers=headers, json=data) # Send Updated IP Address
         res = response.json() # response to json
         if res["success"] == True: # check for success
             print ("IP Address changed to: " + res["result"]["content"]) # Print updated IP
+            print ("Date lats modified: " + res["result"]["modified_on"] + "\n")
         else:
             print ("Check your data.\nPlease see https://api.cloudflare.com/#dns-records-for-a-zone-errors")
             print (res["errors"])
---
+++ config/config.py
@@ -0,0 +1,9 @@
+# change these as you need please see included links for more details
+zoneID = 'Zone-ID-For-Domain' # Getting Zone ID's https://api.cloudflare.com/#zone-list-zones
+dnsID = 'DNS-ID-For-SubDomain' # Getting DNS Record ID's https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
+# Put together our Header
+headers = {
+    'X-Auth-Email': 'Your-Email-Address', # The email you use to login into Cloudflare
+    'X-Auth-Key': 'Global-API-Key', # The Global API Key found here --> https://dash.cloudflare.com/profile/api-tokens
+    'Content-Type': 'application/json' # Type of data to be passed must be application/json
+}
