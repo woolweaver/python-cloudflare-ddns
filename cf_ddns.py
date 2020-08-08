ef87f4443d5c1f2d91be2c520f6e05ad64956e2b
mwoolweaver <michael@woolweaver.bid>
2020-08-07 23:18:13 -0500

    organize

--- cf_ddns.py
+++ cf_ddns.py
@@ -1,68 +1,46 @@
 #!/usr/bin/env python3
 
-import requests
+from requests import get, put
 
 # change these as you need please see here for more deatils
 
-# Getting Zone ID's https://api.cloudflare.com/#zone-list-zones
+zoneID = 'Zone-ID-For-Domain' # Getting Zone ID's https://api.cloudflare.com/#zone-list-zones
 
-zoneID = 'Zone-ID-For-Domain'
-
-# Getting DNS record ID https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
-
-dnsID = 'DNS-ID-For-SubDomain'
-
-# Do Not Change This
-url = "https://api.cloudflare.com/client/v4/zones/" + zoneID + "/dns_records/" + dnsID
-
-# change these as you need please see here for more details
-
-# https://dash.cloudflare.com/profile/api-tokens
-
-# X-Auth-Key is your Global API Key
+dnsID = 'DNS-ID-For-SubDomain' # Getting DNS record ID https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
 
 headers = {
-    'X-Auth-Email': 'Your-Email-Address',
-    'X-Auth-Key': 'Global-API-Key',
-    'Content-Type': 'application/json'
+    'X-Auth-Email': 'Your-Email-Address', # The email you use to login into Cloudflare
+    'X-Auth-Key': 'Global-API-Key', # The Global API Key found here --> https://dash.cloudflare.com/profile/api-tokens
+    'Content-Type': 'application/json' # Type of data to be passed must be application/json
 }
 
-ip = requests.get('https://diagnostic.opendns.com/myip')
-new_ip = ip.text
-
-# change these as you need please see here for more details
+ip = get('https://diagnostic.opendns.com/myip') # fetch our current IP address
+current_ip = ip.text
 
 # https://api.cloudflare.com/#dns-records-for-a-zone-update-dns-record
 
 data = {
-    "type":"A",
-    "name":"FQDN-That-You-Want-Update",
-    "content":new_ip,
-    "ttl":'1',
-    "proxied":'false'
+    "type":"A", # record type A or AAA
+    "name":"FQDN-That-You-Want-Update", # example.com
+    "content":current_ip, # The IP address we just fetched from opendns.com
+    "ttl":'1', # set to 1 for AUTO 
+    "proxied":'false' # whether or not to proxy site thru cloudflare
 }
 
-check_cf_IP = requests.get(url, headers=headers)
+# Put together Cloudflare URL with zoneID and dnsID
+url = "https://api.cloudflare.com/client/v4/zones/" + zoneID + "/dns_records/" + dnsID
 
-check = check_cf_IP.json()
+check_cf_IP = get(url, headers=headers) # GET Info for the exact domain we want to update
+check = check_cf_IP.json() # convert to json
+old_cf_ip = check["result"]["content"] # Find the IP Cloudflare has right now
 
-old_cf_ip = check["result"]["content"]
+print ("Old Cloudflare IP: " + old_cf_ip + "\n")
+print ("Your Current IP  : " + current_ip + "\n")
 
-if old_cf_ip == new_ip:
-
-    print ("Cloudflare IP: " + old_cf_ip + "\n")
-
-    print ("Your IP: " + new_ip + "\n")
-
+if old_cf_ip == current_ip: # Check if our current IP matches what Cloudflare has
     print ("IP address hasn't changed")
 
 else:
-    response = requests.put(url, headers=headers, json=data)
-
+    response = put(url, headers=headers, json=data) # Send Updated IP Address
     res = response.json()
-
-    print ("Cloudflare IP: " + old_cf_ip + "\n")
-
-    print ("Your IP: " + new_ip + "\n")
-
     print ("IP Address changed to: " + res["result"]["content"])
