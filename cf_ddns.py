d9a80a73e2d396062b7d4a74a50a276d03095aae
Michael Woolweaver <m.woolweaver@icloud.com>
2020-08-08 02:31:07 -0500

    Rearrange variables to match readme.md order
    Figured this might make understanding the script a bit easier

--- cf_ddns.py
+++ cf_ddns.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 from requests import get, put
 # change these as you need please see included links for more details
-zoneID = 'Zone-ID-For-Domain' # Getting Zone ID's https://api.cloudflare.com/#zone-list-zones
-dnsID = 'DNS-ID-For-SubDomain' # Getting DNS Record ID's https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
 # Put together our Header
 headers = {
     'X-Auth-Email': 'Your-Email-Address', # The email you use to login into Cloudflare
     'X-Auth-Key': 'Global-API-Key', # The Global API Key found here --> https://dash.cloudflare.com/profile/api-tokens
     'Content-Type': 'application/json' # Type of data to be passed must be application/json
 }
+zoneID = 'Zone-ID-For-Domain' # Getting Zone ID's https://api.cloudflare.com/#zone-list-zones
+dnsID = 'DNS-ID-For-SubDomain' # Getting DNS Record ID's https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
 # We need to find our IP
 ip = get('https://diagnostic.opendns.com/myip') # fetch our current IP address
 current_ip = ip.text # we only need the text on the page
